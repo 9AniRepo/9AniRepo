2b40a2d32f34ae2940c4de69000bdd5f6888c9ea
Asianman132 <gameplayer4467@gmail.com>
2021-03-21 13:03:38 -0400

    added some progress checks due to most verbose output disabled

--- updater.py
+++ updater.py
@@ -28,11 +28,21 @@ url = filtered[1]	# dl link for the file
 
 # create shell script to install NineAnimator
 with open('installer.sh', 'w') as f:	# create shell script and sets f to write mode
+    f.write('echo "Downloading app archive"')
 	f.write('wget -q --no-check-certificate --content-disposition %s\n' % url)	# downloads nightly zip
+    f.write('echo "Download complete"')
+    f.write('echo "Unzipping archive"')
 	f.write('unzip -q %s.zip\n' % name)	# unzip which outpus the ipa
+    f.write('echo "Unzipping complete"')
 	f.write('mv %s.ipa app.zip\n' % name) # rename ipa
+    f.write('echo "Renamed output file to ipa"')
+    f.write('echo "Unzipping ipa"')
 	f.write('unzip -q app.zip\n')	# unzip which outputs the Payload
+    f.write('echo "Unzipping complete"')
+    f.write('echo "Resigning app"')
 	f.write('ldid Payload/*.app\n') # run ldid on the Payload and .app folder
+    f.write('echo "Resign complete"')
+    f.write('echo "Rezipping app"')
 	f.write('zip -qr output.ipa Payload\n') # rezip the Payload folder into ipa
 	f.write('appinst output.ipa\n') # install ipa via appsync using appinst
 
